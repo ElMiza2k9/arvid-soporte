a6f62b21c8304b669680dc2df9524cc5e44189c9
Andres <82073118+fwdlz@users.noreply.github.com>
2021-11-23 19:13:08 -0500

    eval.js

--- src/commands/admin/eval.js
+++ src/commands/admin/eval.js
@@ -20,34 +20,44 @@ module.exports = {
      */
     run: async (client, interaction, args) => {
         const code = interaction.options.getString('codigo')
-        if (!interaction.user.id === '673282658679259197') return interaction.editReply({ content: "No puedes usar este comando."});
+        if (!interaction.user.id === '673282658679259197') return interaction.editReply({ content: "No puedes usar este comando." });
 
         try {
             const evaled = eval(code);
 
-          const embed = new MessageEmbed()
-          .setAuthor(interaction.user.tag, interaction.user.displayAvatarURL())
-          .setColor('GREEN')
-          .setTitle("Codigo ejecutado correctamente.")
-          .addFields({
-              name: "**Tipo:**",
-              value: `\`\`\`prolog\n${typeof(evaled)}\`\`\``,
-              inline: true,
-              
-          }, {
-              name: "**Evaluado en:**",
-              value: `\`\`\`yaml\n${Date.now() - interaction.createdTimestamp}ms\`\`\``,
-              inline: true,
-          }, {
-              name: "**Entrada:**",
-              value: `\`\`\`js\n${code}\`\`\``,
-          }, {
-              name: "**Salida:**",
-              value: `\`\`\`js\n${inspect(evaled, { depth: 0 })}\`\`\``,
-          })
-           interaction.editReply({ embeds: [embed] })
+            const embed = new MessageEmbed()
+                .setColor('GREEN')
+                .setTitle("Successful")
+                .addFields({
+                    name: "**Tipo:**",
+                    value: `\`\`\`prolog\n${typeof (evaled)}\`\`\``,
+                    inline: true,
+
+                }, {
+                    name: "**Evaluado en:**",
+                    value: `\`\`\`yaml\n${Date.now() - interaction.createdTimestamp}ms\`\`\``,
+                    inline: true,
+                }, {
+                    name: "**Entrada:**",
+                    value: `\`\`\`js\n${code}\`\`\``,
+                }, {
+                    name: "**Salida:**",
+                    value: `\`\`\`js\n${inspect(evaled, { depth: 0 })}\`\`\``,
+                })
+            interaction.editReply({ embeds: [embed] })
         } catch (err) {
-            
+            const errorEmbed = new MessageEmbed()
+                .setColor("RED")
+                .addFields({
+                    name: "**Entrada:**",
+                    value: `\`\`\`js\n${code}\`\`\``,
+                }, {
+                    name: "**Salida:**",
+                    value: `\`\`\`js\n${err}\`\`\``,
+                })
+                .setTitle("Error")
+
+                interaction.editReply({ embeds: [errorEmbed] });
         }
 
     }
